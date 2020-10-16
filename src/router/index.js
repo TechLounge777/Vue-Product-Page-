b602e74dabcf3922c6bfe46d0fc9aefcbf41cbef
TechLounge777 <msblanche777@gmail.com>
2020-10-16 18:08:30 +0200

    added home and about routes and views

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,21 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
+import HomeView from '../views/HomeView.vue';
+
+const NotFoundView = () => import('../views/NotFoundView.vue');
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home,
+    name: 'HomeView',
+    component: HomeView,
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    path: '*',
+    name: 'NotFoundView',
+    component: NotFoundView,
   },
 ];
 
---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,16 @@
+<template>
+<div class="view">
+  <h1>Page Not Found</h1>
+  <p>Check out our current <router-link to="/">promotions</router-link></p>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'NotFoundView',
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
